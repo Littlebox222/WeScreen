103625582529994b5876d5ae3b8fe36e2cff4f2e
Littlebox222 <wangxin198797@yahoo.com.cn>
2014-12-23 11:59:53 +0800

    update

---
+++ WeScreen/Weibo/Model/HttpBaseModel.m
@@ -0,0 +1,57 @@
+//
+//  HttpBaseModel.m
+//  SinaWeibo
+//
+//  Created by Stephy_xue on 13-9-26.
+//  Copyright (c) 2013年 Stephy_xue. All rights reserved.
+//
+
+#import "HttpBaseModel.h"
+
+@implementation HttpBaseModel
+
++ (void)getDataResponseHostName:(NSString *)hostName
+                           Path:(NSString *)path
+                         params:(NSMutableDictionary*) params
+                     httpMethod:(NSString*)method
+                   onCompletion:(void (^)(NSData *responseData))completionBlock
+                        onError:(MKNKErrorBlock)errorBlock
+{
+    MKNetworkEngine *engine = [[MKNetworkEngine alloc] initWithHostName:hostName];
+    
+    MKNetworkOperation *op =[engine operationWithPath:path params:params httpMethod:method ssl:YES];
+    
+    [op addCompletionHandler:^(MKNetworkOperation *operation){
+        NSData *responseData = [operation responseData];
+        completionBlock(responseData);
+    } errorHandler:^(MKNetworkOperation* completedOperation, NSError* error){
+        errorBlock(error);
+    }];
+    
+    [engine enqueueOperation:op];
+}
+
++ (void)uploadImageHostName:(NSString *)hostName
+                       Path:(NSString *)path
+                      image:(NSData *)imageData
+                     params:(NSMutableDictionary*) params
+                 httpMethod:(NSString*)method
+               onCompletion:(void (^)(NSData *responseData))completionBlock
+                    onError:(MKNKErrorBlock)errorBlock
+{
+    MKNetworkEngine *engine = [[MKNetworkEngine alloc] initWithHostName:hostName];
+    
+    MKNetworkOperation *op =[engine operationWithPath:path params:params httpMethod:method ssl:YES];
+    [op addData:imageData forKey:@"pic"];
+    [op addCompletionHandler:^(MKNetworkOperation *operation){
+        NSData *responseData = [operation responseData];
+        completionBlock(responseData);
+    } errorHandler:^(MKNetworkOperation* completedOperation, NSError* error){
+        errorBlock(error);
+    }];
+    
+    [engine enqueueOperation:op];
+}
+
+
+@end
